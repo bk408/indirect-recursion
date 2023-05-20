27cdb6217d1e748715a99b205125c9a73c37671f
bk408 <bhavyakhatri1912@gmail.com>
2023-05-20 22:42:24 +0530

    indirect recursion

---
+++ script.js
@@ -0,0 +1,18 @@
+let money = 100;
+let totalApple = 0;
+
+function buyApple(x){
+    if(x>0){
+        console.log("I have", x , "RS", totalApple)
+        buyMore(x);
+    } else{
+        console.log("I don't have more money", totalApple);
+    }
+}
+
+function buyMore(x){
+    totalApple++
+    buyApple(x-5);
+}
+
+buyApple(money);
